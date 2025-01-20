5aba98ab5a8fef7d72bbd480d2c76693f5d44870
Nama Anda <you@example.com>
2025-01-20 19:26:38 +0700

    first commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <p>Sistem Manajamen Bank Sampah Inayah | Repost by rzkyyymhmd</a></p>
+
+        </nav>
+    </div>
+</footer>
